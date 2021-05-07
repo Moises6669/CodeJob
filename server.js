34ab08ceae8be49631401fe48ecfbcf8f545f96f
Nobody <moisesalex132@gmail.com>
2021-05-07 11:15:57 -0600

    ➕ADD: configuracion de archivos estaticos

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
+const path = require('path');
 const app = express();
 
 app.listen(process.env.PORT || 4000, () => {
     console.log("server live 4000");
 });
+
+//settings
+app.engine('handlebars', exphbs({
+    defaultLayout: 'layuot'
+}));
+app.set('view engine', 'handlebars');
+
+//middlewares
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
 
 //routes
 app.use(require('./routes/index.routes'));
