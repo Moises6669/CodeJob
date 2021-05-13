e1e818b606a4dc001c4c3efe808b4a977f1ea895
= <=>
2021-05-12 22:26:58 -0600

    ➕ ADD: seleccionador de tegnologias

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,21 @@
- 
+document.addEventListener("DOMContentLoaded", () => {
+  const skills = document.querySelector(".lista-conocimientos");
+  if (skills) {
+    skills.addEventListener("click", agregarSkills);
+  }
+});
+
+const skills = new Set();
+
+const agregarSkills = (e) => {
+  if (e.target.classList.contains("activo")) {
+    skills.add(e.target.textContent);
+    e.target.classList.remove("activo");
+  } else {
+    skills.add(e.target.textContent);
+    e.target.classList.add("activo");
+    console.log(skills);
+  }
+  const skillsArray = [...skills];
+  document.querySelector("#skills").value = skillsArray;
+};
