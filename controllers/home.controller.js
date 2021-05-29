98694d6fb740744a8b56361998024cb87059a42e
Nobody <moisesalex132@gmail.com>
2021-05-28 22:03:14 -0600

    ➕ADD: vistas de vacantes de la BDS

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,8 +1,18 @@
-exports.mostrarTrabajos = (req, res) => {
+const Vacante = require('../models/vacantes');
+
+exports.mostrarTrabajos = async (req, res,next) => {
+    
+    const vacantes = await Vacante.find().lean();
+
+    if(!vacantes) return next();
+
     res.render('home', {
         nombrePagina: 'CodeJobs',
         tagline: 'Encuentra y publica trabajos para desarrolladores de todos los tipos',
         barra:true,
-        boton:true
+        boton:true,
+        vacantes:vacantes
     })
+
+ 
 }
